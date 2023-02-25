73e01315e2c84b2384dab40499cb15c4ca7a00f5
Christopher Munoz <36735312+chrismunoz92@users.noreply.github.com>
2023-02-24 19:05:41 -0700

    Initial project setup

---
+++ app/ingredient.py
@@ -0,0 +1,5 @@
+class Ingredient:
+    def __init__(self, name: str = None, unitNumber: int = None, unitName: str = None):
+        self.name = name
+        self.unitNumber = unitNumber
+        self.unitName = unitName
